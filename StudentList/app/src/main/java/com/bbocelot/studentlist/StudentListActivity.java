9757545c17429217d7c9d2e266fae925e9c0d222
Stanislava <ferrashe4ka@mail.ru>
2020-08-13 19:49:31 +0300

    Student detailed view added and working

--- StudentList/app/src/main/java/com/bbocelot/studentlist/StudentListActivity.java
+++ StudentList/app/src/main/java/com/bbocelot/studentlist/StudentListActivity.java
@@ -1,6 +1,9 @@
 package com.bbocelot.studentlist;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -32,12 +35,20 @@ public class StudentListActivity extends AppCompatActivity {
     }
 
     private void onStudentClick(Student student) {
-        Toast.makeText(StudentListActivity.this, student.name+student.surname, Toast.LENGTH_SHORT).show();
+        EditText editTextName = findViewById(R.id.detailed_student__tv_name);
+        EditText editTextSurname = findViewById(R.id.detailed_student__tv_surname);
+        CheckBox checkBox = findViewById(R.id.detailed_student__tv_sex);
+        ImageView avatar = findViewById(R.id.detailed_student__iv_avatar);
+        editTextName.setText(student.name);
+        editTextSurname.setText(student.surname);
+        checkBox.setChecked(student.sex);
+        avatar.setImageResource(student.avatar);
+//        Toast.makeText(StudentListActivity.this, student.name+student.surname, Toast.LENGTH_SHORT).show();
     }
 
     private List<Student> generateStudentsList() {
         List<Student> students = new ArrayList<>();
-        students.add(new Student("Мисс","Марпл",false, 0));
+        students.add(new Student("Мисс","Марпл",false, R.drawable.siesta_the_cat));
         students.add(new Student("Эркюль","Пуаро",true, R.drawable.silver_the_cat));
         students.add(new Student("Жюль","Мегре",true, R.drawable.silver_the_cat));
         return students;
